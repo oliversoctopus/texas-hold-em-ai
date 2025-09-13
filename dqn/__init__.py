adc543a933fe1af052e1b786c34f12ea13af0116
Oliver Fan <oliver.s.fan@gmail.com>
2025-09-12 20:28:10 -0500

    Reorganize and deprecate DQN training components
    Move DQN files to dqn/ directory and add deprecation warnings for training functions

---
+++ dqn/__init__.py
@@ -0,0 +1,8 @@
+"""
+Deep Q-Network (DQN) poker AI - evaluation only (training deprecated)
+"""
+
+from .poker_ai import PokerAI
+from .neural_network import PokerNet
+
+__all__ = ['PokerAI', 'PokerNet']
